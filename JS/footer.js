d36cecf4227c1cf8b99e9b35c0ff6a4ee48a6453
Samuel Bacconin <samuel.bacconin@live.fr>
2022-11-13 13:11:02 +0100

    footer responsive

--- JS/footer.js
+++ JS/footer.js
@@ -1,9 +1,10 @@
 const categories = document.querySelectorAll(".footer__cat");
+var width = window.screen.width;
 
 categories.forEach(category => {
   category.onclick = () => {
 
-    if(category.classList.contains("open")) {
+    if(category.classList.contains("open") && width <= 768) {
       category.classList.remove("open");
       category.children[1].style.display = "none";
     } else {
